61fb25482503b9afc3cd519dcde73b6e4098bb06
AyatMostafa <45332061+AyatMostafa@users.noreply.github.com>
2019-03-16 21:47:53 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <c11></c11>
-    <document></document> 
-  <!--  <navBar></navBar> -->
+  <!--  <c11></c11>
+    <document></document> -->
+    <navBar></navBar>
     
   </div>
 </template>
@@ -43,7 +43,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   background-color: #DAE0E6;
   width:100%;
