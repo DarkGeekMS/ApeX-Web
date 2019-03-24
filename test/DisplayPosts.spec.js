561a5ac9d6379b52016276de355a5bc1d23a44b5
Marc Nagui <45571009+MarcNagui@users.noreply.github.com>
2019-03-24 02:20:00 +0200

    DisplayPosts Mimic service

---
+++ test/DisplayPosts.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import DisplayPosts from '../src/components/DisplayPosts.vue';
+import expect from 'expect';
+import moxios from 'moxios'
+
+describe ('DisplayPosts' , () =>{
+    let wrapper = mount(DisplayPosts);
+    it('default postInfo to be null' , () =>{
+ 		expect(wrapper.vm.postInfo).toBe({});
+    });
+
+
+
+});
