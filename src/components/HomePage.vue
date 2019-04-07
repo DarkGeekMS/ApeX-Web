2a680d26f84a11b5f216c3e94cfa54321fa6ac8e
AyatMostafa <45332061+AyatMostafa@users.noreply.github.com>
2019-04-07 11:07:35 +0200

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="HomePage" >
-     <nav-bar></nav-bar>
      <hotposts></hotposts>
      <DisplayPosts></DisplayPosts>
 
@@ -10,19 +9,12 @@
 </template>
 
 <script>
-
-
-import component1 from "./component1.vue"
 import DisplayPosts from "./DisplayPosts.vue"
-import NavBar from './NavBar.vue'
 import hotposts from './HotPosts.vue'
-
 
 export default {
 
   components:{
-    'c11':component1,
-    'navBar': NavBar,
     'hotposts':hotposts,
     'DisplayPosts':DisplayPosts,
 
@@ -30,9 +22,7 @@ export default {
   name: 'app',
   data(){
     return{
-      title:'title ',
-      c11:component1,
-      document:docu,
+      title:'title '
     }
   }
 }
